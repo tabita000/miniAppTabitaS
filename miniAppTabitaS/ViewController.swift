af301708ea09e47835b1762f305b7a1612abb606
Tabita Sadiq <tfadiq@students.mchenry.edu>
2024-02-25 19:55:56 -0600

    Initial Commit

---
+++ miniAppTabitaS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  miniAppTabitaS
+//
+//  Created by Tabita Sadiq on 2/25/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
